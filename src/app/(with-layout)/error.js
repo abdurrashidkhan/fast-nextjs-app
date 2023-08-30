a876b2c50e4178b3970efe4ac0cfb267e1b4ea02
abdurrashidkhan <rashidkhanbd57@gmail.com>
2023-08-30 11:44:09 +0600

    update here

---
+++ src/app/(with-layout)/error.js
@@ -0,0 +1,16 @@
+"use client"
+import { useEffect } from "react";
+
+const error = ({ error, reset }) => {
+  useEffect(() => {
+    console.error(error.message)
+  }, [error])
+  console.log(error.message)
+  return (
+    <div>
+      <h1>{error.message}</h1>
+    </div>
+  );
+}
+
+export default error;
---
+++ src/app/(with-layout)/gallery/loading.js
@@ -0,0 +1,9 @@
+const loading = () => {
+  return (
+    <div className="absolute top-[50%] left-[50%]">
+      <h1>loading...</h1>
+    </div>
+  );
+}
+
+export default loading;
